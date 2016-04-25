626f2a7d524e6cc46390f8565a4d0d7df87e345a
Andreas Neumann <andneuma@posteo.de>
2016-04-25 19:54:51 +0200

    DRY up place input via _form-partial, split up index and edit places, show geocoords in places index table

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,12 @@
 class Place < ActiveRecord::Base
   has_many :descriptions, :dependent => :delete_all
-  geocoded_by :address
+  geocoded_by :address # <- virtual attribute defined below
+  after_validation :geocode
+
   accepts_nested_attributes_for :descriptions
 
   def address
-    [self.street, self.house_number, self.postal_code, self.city].join(, )
+    [self.street, self.house_number, self.postal_code, self.city].join(", ")
   end
 
   def geojson

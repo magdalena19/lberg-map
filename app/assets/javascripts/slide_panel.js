818739bd4048b8fe8d5d3e042651cda3fa74d044
magdalena19 <jo5cha@web.de>
2016-06-28 23:36:02 +0200

    replace modals with custom slide panel

---
+++ app/assets/javascripts/slide_panel.js
@@ -0,0 +1,56 @@
+jQuery(function() {
+  var slidePanels = jQuery('.slidepanel');
+
+  slidePanels.each(function() {
+    var panel = jQuery(this);
+    panel.bind('open', function() {
+      closeAllPanels();
+      panel.animate({bottom: '10'}, 300);
+      panel.addClass('slidx-open');
+      jQuery('.control-container').hide();
+    });
+    panel.bind('close', function() {
+      panel.css('bottom', '-' + panel.outerHeight() + 'px');
+      panel.removeClass('slidx-open');
+      jQuery('.control-container').show();
+    });
+
+    closeAllPanels = function() {
+      slidePanels.each(function() {
+        jQuery(this).trigger('close');
+      });
+    };
+  });
+
+  resizePanels = function() {
+    slidePanels.each(function() {
+      var panel = jQuery(this);
+      var height = jQuery(window).height() - 160;
+      var width = jQuery(window).width() - 20;
+      panel.find('.content').css('max-height', height + 'px');
+      panel.width(width);
+      if (!panel.hasClass('slidx-open')) {
+        panel.css('bottom', '-' + panel.outerHeight() + 'px');
+      };
+    });
+  };
+
+  jQuery(window).resize(function() {
+    resizePanels();
+  }).resize();
+
+  jQuery('.slidepanel-button').click(function() {
+    var panelName = jQuery(this).attr('slidepanel')
+    var panel = jQuery('.' + panelName);
+    if (!panel.hasClass('slidx-open')) {
+      panel.trigger('open');
+    }
+    else {
+      panel.trigger('close');
+    }
+  });
+
+  jQuery('.hide-slidepanel').click(function() {
+    closeAllPanels();
+  });
+});

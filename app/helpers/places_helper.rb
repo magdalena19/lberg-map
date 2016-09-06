82804f6c1cafe28800361dd1ae5a9b65183a31a2
andneuma <andneuma@posteo.de>
2016-09-06 16:34:03 +0200

    Changes to address display helper

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -6,4 +6,8 @@ module PlacesHelper
   def last_places_created
     Place.all.sort_by(&:created_at).reverse
   end
+
+  def address(place)
+    ["#{place.street} #{place.house_number}", "#{place.postal_code} #{place.city}"].select(&:present?).join(', ')
+  end
 end

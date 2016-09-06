8a5b6949b0be8d998ecca96c3fe25b86015e2751
andneuma <andneuma@posteo.de>
2016-09-06 18:23:18 +0200

    Change params hash modification in modified_params method inside places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -69,11 +69,12 @@ class PlacesController < ApplicationController
 
   def save_update
     # Ugly: lat/lon have to be inserted into modified_params-hash in order to make update_attributes work...
-    if @place.latitude && @place.longitude
-      modified_params[:latitude] = @place.latitude
-      modified_params[:longitude] = @place.longitude
+    params_for_update = modified_params
+    if @place.lat_lon_present?
+      params_for_update[:latitude] = @place.latitude
+      params_for_update[:longitude] = @place.longitude
     end
-    if @place.update_attributes(modified_params)
+    if @place.update_attributes(params_for_update)
       flash[:success] = 'Changes saved! Wait for review...'
       redirect_to places_path
     else
@@ -83,7 +84,7 @@ class PlacesController < ApplicationController
   end
 
   def save_new
-    # Take lat/lon values from hash passed by create form
+    # Take lat/lon values from params hash passed by create form
     @place.latitude ||= params[:place][:latitude]
     @place.longitude ||= params[:place][:longitude]
     if @place.save

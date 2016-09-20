b2e3400f85c2597c81db99039e9e133b8f0c86e7
andneuma <andneuma@posteo.de>
2016-09-20 09:49:17 +0200

    Add searchable and sortable datatable support for place index via jquery dt plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require dataTables/jquery.dataTables
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require leaflet

454d446b9fe710298b2a7a7b5b0c79e5ecf51b8d
magdalena19 <kjoscha@gmail.com>
2016-10-06 12:52:27 +0200

    delete map.js duplicate
    fix edit button test
    
    re-implement edit-place button click action

--- test/features/places/edit_button_test.rb
+++ test/features/places/edit_button_test.rb
@@ -1,10 +1,32 @@
 require 'test_helper'
 
-feature 'edit buttons' do
-  scenario 'are visible when logged in', js: true do
+feature 'edit button' do
+  before do
+    @category = Category.new(name_en: 'Playground')
+    @category.save
+    Category.new(name_en: 'Hospital').save
+    @place = Place.new(
+      name: 'Magda',
+      street: 'Magdalenenstraße',
+      house_number: '19',
+      postal_code: '10365',
+      city: 'Berlin',
+      categories: @category.id,
+      homepage: 'https://heise.de',
+      email: 'foo@bar.org',
+      phone: '030 2304958',
+      description_en: '<center><b>This is the description.</b></center>',
+      latitude: 52.5,
+      longitude: 13.5,
+      reviewed: true
+    )
+    @place.save
+  end
+
+  scenario 'is visible', js: true do
     login
-    page.find('.show-places').trigger('click')
     sleep(1)
+    page.find('.leaflet-marker-icon').trigger('click')
     page.must_have_css('.edit-place')
   end
 end

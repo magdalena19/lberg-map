d2f0587f7a81c1185260806f96c3f33ef6883d05
p.kitt@d00m.org <p.kitt@d00m.org>
2016-11-12 11:24:08 +0100

    added conf files for docker, supervisord

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# defines which mages should be used, which software installed in the 
+# webapp container
+FROM ruby:slim
+RUN apt-get update -qq && apt-get install -y \
+	build-essential \
+	libpq-dev \
+	nodejs \
+	imagemagick \
+    libsqlite3-dev \
+    supervisor
+RUN mkdir /lbergmap
+WORKDIR /lbergmap
+ADD Gemfile /lbergmap/Gemfile
+ADD Gemfile.lock /lbergmap/Gemfile.lock
+RUN bundle install
+ADD . /lbergmap
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,7 @@ app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
+rails_env = ENV['RAILS_ENV'] || 'production'
 
 # Set unicorn options
 worker_processes 2
@@ -10,7 +11,8 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+#listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen(3000, backlog: 64) #if ENV['RAILS_ENV'] == 'development'
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"

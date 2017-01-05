66efdd3bf4ba90bc674fb877ac7c26f73c2951f5
kjoscha <kjoscha@gmail.com>
2017-01-05 20:24:12 +0100

    set wysihtml5 options

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,6 +46,16 @@ jQuery(function() {
   jQuery('.locale-slidepanel').trigger('open');
 
   jQuery('.wysihtml5').each(function(i, elem) {
-    $(elem).wysihtml5();
+    $(elem).wysihtml5({
+      toolbar: {
+        'font-styles': false,
+        'emphasis': true,
+        'lists': true,
+        'html': true,
+        'link': true,
+        'image': false,
+        'color': false,
+      }
+    });
   });
 });

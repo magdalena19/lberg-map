cb37e07a5e1b01883eff466c7f74d1b58f2f0a69
andneuma <andneuma@posteo.de>
2017-01-06 21:03:27 +0100

    Elim bug risks: Obey eslint on javascript style

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,7 +2,7 @@ jQuery(function(){
   // Deactivate "send copy to sender" option if no email address is present
   $('#message_sender_email').on('input', function(val){
     var current_value = $(this).val();
-    if (current_value != '') {
+    if (current_value !== '') {
       $('.email_reply').show(350);
     } else {
       $('.email_reply').hide(350);

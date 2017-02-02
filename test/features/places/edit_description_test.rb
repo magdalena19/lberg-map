1d864b9dcfde38f569f0f4845549f2763ad2615b
andneuma <andneuma@posteo.de>
2017-02-02 23:35:31 +0100

    Make edit description test work again

--- test/features/places/edit_description_test.rb
+++ test/features/places/edit_description_test.rb
@@ -5,30 +5,20 @@ feature 'Edit description' do
     @place = create(:place, :reviewed)
   end
 
+  # TODO Check: Fails sometimes... oO
   scenario 'Do not show guest edits in place list', :js do
     visit edit_place_path id: @place.id
-    fill_in_description_field('Changed description')
+    find('#description_en').set "Changed description"
     validate_captcha
     click_on('Update Place')
 
     Capybara.reset_sessions!
     assert_equal 1, Place.reviewed_places.count
 
-    sleep(1)
     visit '/places'
     page.must_have_content @place.name
     page.find('.glyphicon-triangle-bottom').trigger('click')
     page.wont_have_content('Changed description')
 
-  end
-
-  private
-
-  def fill_in_description_field(content)
-    # bootsy builds text area via iframe
-    within_frame(find('.wysihtml5-sandbox')) do
-      query = "document.querySelector('.description-area').innerHTML = '#{content}'"
-      page.execute_script(query)
-    end
   end
 end

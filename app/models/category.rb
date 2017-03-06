ae5539ef1f6c8ede97f28af6aa2e4291249d5aac
andneuma <andneuma@posteo.de>
2017-03-06 17:43:17 +0100

    Autotranslates category names

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
+require 'auto_translation/auto_translate'
+
 class Category < ActiveRecord::Base
+  include AutoTranslate
+
   translates :name
   globalize_accessors
+
+  after_create :enqueue_auto_translation
+
+  def enqueue_auto_translation
+    TranslationWorker.perform_async('Category', id) if Admin::Setting.auto_translate
+  end
 
   def self.id_for(category_string)
     category = Category.all.find do |cat|

2eead9318adc2307a10d861a222d8ff25a3e4516
andneuma <andneuma@posteo.de>
2017-03-27 17:02:42 +0200

    Do not choose locale if already set in session

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def choose_locale
+    redirect_to landing_page_url if session[:locale]
   end
 
   def landing_page
+    session[:locale] = locale
   end
 end

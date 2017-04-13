a9f880c89f4521e75f1087d2e86c749b65ced54b
andneuma <andneuma@posteo.de>
2017-04-13 16:42:48 +0200

    Fix event date insertion in event edit form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -80,11 +80,13 @@ jQuery(function() {
   };
 
   jQuery('#is_place').on('click', function(){
-    jQuery('#place_date_form').hide(350);
+    jQuery('#place_start_date').prop('disabled', true);
+    jQuery('#set_end_date').prop('disabled', true);
   });
 
   jQuery('#is_event').on('click', function(){
-    jQuery('#place_date_form').show(350);
+    jQuery('#place_start_date').prop('disabled', false);
+    jQuery('#set_end_date').prop('disabled', false);
     picker(
         jQuery('#place_start_date'),
         jQuery('#set_end_date').is(':checked')
@@ -92,9 +94,21 @@ jQuery(function() {
   });
 
   jQuery('#set_end_date').on('click', function(){
+    var with_end_date = jQuery(this).prop('checked');
+    var date_input = jQuery('#place_start_date');
+    var orig_value = date_input.val();
+    var start_date = orig_value.split(' - ')[0];
+    var end_date = orig_value.split(' - ')[1];
+
+    if (with_end_date === false) {
+      date_input.val(start_date).trigger('change');
+    } else {
+      date_input.val(start_date + ' - ' + start_date).trigger('change');
+    }
+
     picker(
         jQuery('#place_start_date'),
-        jQuery('#set_end_date').is(':checked')
+        with_end_date
         );
   });
 

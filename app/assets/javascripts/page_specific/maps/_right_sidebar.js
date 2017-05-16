b8957ceb474621cd8e7d2613176dbd43af29fd1d
andneuma <andneuma@posteo.de>
2017-05-16 10:22:35 +0200

    Transfer right sidepanel code

---
+++ app/assets/javascripts/page_specific/maps/_right_sidebar.js
@@ -0,0 +1,184 @@
+// RIGHT SIDEBAR FUNCTIONALITY
+
+jQuery(function() {
+  var current_position_icon = L.icon({
+    iconUrl: marker_current_position
+  });
+
+  // INSERT CONFIRMATION ACTION
+  var locationMarker;
+
+  function confirmPlaceInsert(lat, lon, geocoding_result) {
+    // display marker
+    map.setView([lat, lon], 18);
+    if (locationMarker) {
+      map.removeLayer(locationMarker);
+    }
+    locationMarker = L.circleMarker([lat, lon]).addTo(map);
+
+    // confirmation button
+    jQuery('.confirmation-button-container').fadeIn();
+    jQuery('#confirmation-button-yes').click(function() {
+      var address = {latitude: lat, longitude: lon};
+      if (geocoding_result) {
+        jQuery.extend(address, geocoding_result.properties.address);
+        hideAddressSearchBar();
+      }
+
+      jQuery('.confirmation-button-container').fadeOut();
+      url = '/' + window.map_token + '/places/new?';
+
+      // Generate URL
+      jQuery.each(address, function(prop) {
+        url += prop + '=' + address[prop] + '&';
+      });
+
+      // Redirect
+      window.location.href = url;
+    });
+
+    // cancel button
+    jQuery('#confirmation-button-no').click(function() {
+      jQuery('.confirmation-button-container').fadeOut();
+      map.removeLayer(locationMarker);
+      jQuery('.leaflet-overlay-pane').css('cursor', 'inherit');
+      showMapElements();
+      fit_to_bbox();
+    });
+  }
+
+  // INSERT PLACE MANUALLY
+  jQuery('.add-place-manually').on('click', function() {
+    window.location.href = '/' + window.map_token + '/places/new';
+  });
+
+  // ADD PLACE VIA ONCLICK
+  jQuery('.add-place-via-click').click(function(){
+    hideMapElements();
+    jQuery('.leaflet-overlay-pane').css('cursor','crosshair');
+    map.on('click', function(point) {
+      confirmPlaceInsert(point.latlng.lat, point.latlng.lng);
+    });
+  });
+
+
+  // ADD PLACE VIA CURRENT GEOLOCATION
+  jQuery('.add-place-via-location').click(function(){
+    hideMapElements();
+    jQuery('.leaflet-overlay-pane').css('cursor','crosshair');
+    function confirmation(position) {
+      confirmPlaceInsert(position.coords.latitude, position.coords.longitude);
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(confirmation);
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  });
+
+  // ADD PLACE VIA ADDRESS SEARCH
+  jQuery('.add-place-via-address-search').on('click', 'a', function() {
+    hideMapElements();
+
+    geocode_field = L.Control.geocoder({
+      position: 'bottomright',
+      collapsed: false
+    }).addTo(map);
+
+    showAddressSearchBar();
+
+    geocode_field.markGeocode = function(geocoding_result) {
+      lat = geocoding_result.properties.lat;
+      lon = geocoding_result.properties.lon;
+      hideAddressSearchBar();
+      confirmPlaceInsert(lat, lon, geocoding_result);
+    };
+  });
+
+  jQuery('.fade-background').on('click', function() {
+    hideAddressSearchBar();
+    showMapElements();
+  });
+
+  // Reset view to bbox of current place selection
+  function fit_to_bbox() {
+    if (cluster.getLayers().length > 0) {
+      map.fitBounds(cluster.getBounds());
+    }
+  }
+
+  jQuery('.zoom-to-bbox').on('click', function() {
+    fit_to_bbox();
+  });
+
+  // Show / Hide geolocation
+  function zoomTo(lat, lon) {
+    map.setView([lat, lon], 18);
+  }
+
+  jQuery('.toggle-show-geolocation').on('click', function() {
+    function showPosition(position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      current_location = L.marker([lat, lon], {icon: current_position_icon}).addTo(map);
+      L.DomUtil.addClass(current_location._icon, 'current_location_marker');
+      map.current_location = current_location;
+      jQuery('.toggle-show-geolocation').toggleClass('inactive');
+
+      // Zoom in if no POIs on map
+      if (cluster.getLayers().length === 0) {
+        zoomTo(lat, lon);
+      }
+    }
+
+    function getAndShowPosition(){
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        alert('Geolocation is not supported by this browser.');
+      }
+    }
+
+    if (map.current_location === undefined) {
+      getAndShowPosition();
+    } else {
+      map.removeLayer(map.current_location);
+      jQuery('.current_location_marker').remove();
+      map.current_location = undefined;
+      jQuery('.toggle-show-geolocation').toggleClass('inactive');
+    }
+  });
+  // Toggle map info modal
+  jQuery('.show-map-description').on('click', function() {
+    jQuery('.map-description-modal').modal().show();
+  });
+
+  jQuery('.control-button[tray]').on('click', function(){
+    jQuery('.right-sidebar-tray').hide();
+    var trayName = jQuery(this).attr('tray');
+    var tray = jQuery(trayName);
+    var trayClosed = tray.hasClass('closed');
+
+    if (trayClosed) {
+      tray.removeClass('closed').show();
+    } else {
+      tray.addClass('closed').hide();
+    }
+  });
+
+  // Toggle tile layer selection
+  jQuery('.select-tile-layer').on('click', function(){
+    var url = jQuery(this).data('url');
+    var attr = jQuery(this).data('attr');
+    oldLayer = map.baseLayer;
+    map.baseLayer = L.tileLayer(url, {attribution: attr});
+    map.addLayer(map.baseLayer);
+    map.removeLayer(oldLayer);
+    jQuery('.tile-layers').toggleClass('hidden');
+  });
+
+  jQuery(this).on('click', '#map', function() {
+    hideMapTrays();
+  });
+})

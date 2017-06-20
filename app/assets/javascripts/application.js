7d8f2f539fc05710cd267bca39b059716b906d85
andneuma <andneuma@posteo.de>
2017-06-20 13:41:02 +0200

    Restructure and refactor CSS files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -255,6 +255,7 @@ jQuery(function() {
   // Toggle map elements if modal action is triggered
   jQuery('.map-container .modal').on('hidden.bs.modal', function() {
     showMapElements();
+    hideSidepanel();
   });
 
   jQuery('.map-container .modal').on('show.bs.modal', function() {

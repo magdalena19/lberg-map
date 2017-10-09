27b9a6311d6c716293aa77ccf9e92041fdd4525a
andneuma <andneuma@posteo.de>
2017-10-09 13:31:04 +0200

    Fix specs

--- spec/features/events/edit_event_spec.rb
+++ spec/features/events/edit_event_spec.rb
@@ -1,9 +1,20 @@
 feature 'Edit event', :js do
   scenario 'event checkbox is checked when place is event' do
-    event = create :event, :future, map: create(:map, :full_public)
-    visit edit_place_path(id: event.id, map_token: event.map.public_token)
-    page.find(:css, '.date-information-header').trigger('click')
+    map = create(:map, :full_public)
+    event = create(:event, :reviewed,
+      name: 'PartySafari',
+      categories_string: 'Party',
+      start_date: DateTime.new(2015,7,1,20,0),
+      end_date: DateTime.new(2015,7,2,12,0),
+      phone: 110,
+      map: map)
 
+    visit map_path(map_token: map.secret_token)
+    fill_in('search-date-input', with: '01.06.2015 20:00 - 05.07.2015 12:00')
+    click_on 'Apply'
+    open_edit_place_modal(id: event.id)
+    page.find(:css, '.date-information-header').trigger('click')
+#
     expect(page.find('#is_event')).to be_checked
   end
 end
--- spec/features/reviewing/review_place_spec.rb
+++ spec/features/reviewing/review_place_spec.rb
@@ -1,32 +1,43 @@
-feature 'Review place' do
-  before do
-    @map = create :map, :full_public
-    @place = create :place, :reviewed, map: @map
+feature 'Review place', :js do
+  before(:each) do
+    @map = create :map, :full_public 
+    @place = create :place, :reviewed, name: 'Some reviewed place', map: @map
   end
 
-  scenario 'Do not show user edits in review index', :js do
-    login_as_user
-    visit edit_place_path id: @place.id, map_token: @map.secret_token
-    fill_in('place_name', with: 'USER CHANGE')
-    click_on('Update Place')
-    visit places_review_index_path(map_token: @map.secret_token)
+  context 'Privileged user' do
+    scenario 'user inputs do not have to be reviewed' do
+      visit map_path(map_token: @map.secret_token)
+      open_edit_place_modal(id: @place.id)
+      fill_in('place_name', with: 'USER CHANGE')
+      click_on('Update Place')
+      visit places_review_index_path(map_token: @map.secret_token)
 
-    expect(page).not_to have_content('USER CHANGE')
+      expect(page).not_to have_content('USER CHANGE')
+    end
   end
 
-  scenario 'Show guest edits in review index and review place', :js do
-    visit edit_place_path id: @place.id, map_token: @map.public_token
-    fill_in('place_name', with: 'GUEST CHANGE')
-    
-    click_on('Update Place')
-    login_as_user
-    visit places_review_index_path(map_token: @map.secret_token)
+  feature 'Guest edits' do
+    before do
+      visit map_path(map_token: @map.public_token)
+      open_edit_place_modal(id: @place.id)
+      fill_in('place_name', with: 'GUEST CHANGE')
+      click_on('Update Place')
 
-    expect(page).to have_content('SomeReviewedPlace')
+      sleep(2)
+      login_as_user
+    end
 
-    visit review_place_path id: @place.id, map_token: @map.secret_token
+    scenario 'Show guest edits in review index and review place' do
+      visit places_review_index_path(map_token: @map.secret_token)
 
-    expect(page).to have_content('SomeReviewedPlace')
-    expect(page).to have_content('GUEST CHANGE')
+      expect(find_all('td', text: 'GUEST CHANGE')).not_to be_empty
+    end
+
+    scenario 'Shows correct details to be reviewed' do
+      binding.pry if @place.versions.count == 1
+      visit review_place_path(id: @place.id, map_token: @map.secret_token)
+
+      expect(page).to have_content('GUEST CHANGE')
+    end
   end
 end

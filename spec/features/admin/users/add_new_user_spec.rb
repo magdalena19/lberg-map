e1b79a204fd88a20cbe05ef07110042cd675df28
oiejf873l <oiejf873l@systemli.org>
2017-10-21 13:40:44 +0200

    Simplify admin feature specs

--- spec/features/admin/users/add_new_user_spec.rb
+++ spec/features/admin/users/add_new_user_spec.rb
@@ -1,15 +1,8 @@
-feature 'Add new user', :js do
-  scenario 'can find section on admin dashboard' do
+feature 'Manage users', :js do
+  scenario 'can add new user via admin dashboard without activation tokens' do
     login_as_admin
     visit admin_dashboard_path
     click_on('User management')
-
-    expect(page).to have_content('New User')
-  end
-
-  scenario 'can add new user' do
-    login_as_admin
-    visit sign_up_path
 
     expect(page).not_to have_css('#activation_token')
   end

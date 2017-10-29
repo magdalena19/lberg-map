8fa63f10fd5e266c4682ed3c687616f2289e26f3
oiejf873l <oiejf873l@systemli.org>
2017-10-29 19:15:11 +0100

    BUGFIX: Can delete places via secret link if not logged in

--- spec/features/maps/sidepanel_spec.rb
+++ spec/features/maps/sidepanel_spec.rb
@@ -2,28 +2,22 @@ feature 'Sidepanel', :js do
   context 'Place panel buttons' do
     before do
       @map = create :map, :full_public
-      @place = create :place, :reviewed, map: @map
+      @place = create :place, :reviewed, map: @map, name: 'Place'
+      visit map_path(map_token: @map.secret_token)
+      show_places_list_panel
+      find('.name').trigger('click')
     end
 
     scenario 'can delete place from sidepanel' do
-      skip "Capybara does not trigger action"
-      visit map_path(map_token: @map.secret_token)
-      find('.name').trigger('click')
       page.accept_confirm do
         find('.delete-place').trigger('click')
       end
 
-      expect(page).not_to have_css('.places-list-item')
-    end
+      show_places_list_panel
 
-    scenario 'can delete place from sidepanel' do
-      skip "Capybara does not trigger action"
-      visit map_path(map_token: @map.secret_token)
-      find('.name').trigger('click')
-      find('.edit-place').click
-
-      # expect(current_path).to eq edit_place_path(map_token: @map.secret_token, id: @place.id)
-      expect(page).to have_content 'Edit place'
+      expect(page).to have_content('No places yet!')
+      expect(page).not_to have_content('Place')
+      expect(page).not_to have_css('.leaflet-marker-icon')
     end
   end
 end

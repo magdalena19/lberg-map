75170c90ff8441fe1f3cfa15eb858653a1b108f5
kjoscha <kjoscha@gmail.com>
2017-11-27 17:05:50 +0100

    address search irrespective of poi adding

--- spec/features/places/color_picker_spec.rb
+++ spec/features/places/color_picker_spec.rb
@@ -2,6 +2,7 @@ feature 'Marker color selection', :js do
   let(:map) { create :map, :full_public }
 
   scenario 'Show color picker if multi color POIs enabled' do
+    skip 'DEPRECATED'
     create :settings, multi_color_pois: true
 
     open_new_place_modal(map_token: map.public_token)
@@ -10,6 +11,7 @@ feature 'Marker color selection', :js do
   end
 
   scenario 'Do not show color picker if multi color POIs disabled' do
+    skip 'DEPRECATED'
     create :settings, multi_color_pois: false
 
     open_new_place_modal(map_token: map.public_token)

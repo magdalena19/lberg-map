031399c324cc41c43de098210be9d18419d887e5
Andreas Neumann <andneuma@posteo.de>
2018-04-12 00:42:11 +0200

    Hotfix: Do not alter token values in maps form

--- spec/features/maps/map_form_spec.rb
+++ spec/features/maps/map_form_spec.rb
@@ -1,52 +1,29 @@
 feature 'Map form', js: true do
-  context 'Map creation' do
+  context 'Edit map' do
     before do
       execute_script("jQuery('.footer').css('display', 'none')") # circumvent button finding prob
-      visit new_map_path
+      @map = create :map, :full_public
+      visit edit_map_path(map_token: @map.secret_token)
     end
 
-    scenario 'Check form features' do
-      # SPEC INTERFACE
-      # Base setup
-      click_on('Base setup')
-      fill_in_valid_map_attributes
+    scenario 'it does not reassign public and secret token' do
+      find_correct_secret_token
+      find_correct_public_token
+    end
 
-      # Publication settings
+    private
+
+    def find_correct_secret_token
+      secret_token_field_value = find('#map_secret_token').value
+
+      expect(secret_token_field_value).to eq @map.secret_token
+    end
+
+    def find_correct_public_token
       click_on('Publication settings')
-      set_as_public
-      find('#map_public_token')
-      find('#map_maintainer_email_address')
-      find('label', text: 'Map imprint (optional)')
-      find('#map_allow_guest_commits')
+      public_token_field_value = find('#map_public_token').value
 
-      # Language support
-      click_on('Multi-language support')
-      find("#map_supported_languages_[value='de']").set(true)
-      find("#map_supported_languages_[value='en']").set(false)
-
-      create_map
-      map = Map.find_by(secret_token: 'secret_token')
-
-      expect(map.supported_languages).to eq ['de']
-      expect(map).to be_a(Map)
-      expect(map.is_public).to be true
-      expect(map.user).to eq User.first
+      expect(public_token_field_value).to eq @map.public_token
     end
-  end
-
-  private
-
-  # Map form helpers
-  def fill_in_valid_map_attributes
-    fill_in('map_title', with: 'SomeTitle')
-    fill_in('map_secret_token', with: 'secret_token')
-  end
-
-  def set_as_public
-    publish_checkbox.trigger('click') unless publish_checkbox.checked?
-  end
-
-  def publish_checkbox
-    find('#map_is_public')
   end
 end

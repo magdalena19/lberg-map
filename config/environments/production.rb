c6b4d356a9fc702bf2669511cddcef0916506f28
Andreas Neumann <andneuma@posteo.de>
2018-04-18 12:53:40 +0200

    Receive app host via environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_controller.default_url_options = { host: 'lpkb.de' }
+  config.action_controller.default_url_options = { host: ENV['app_hostname'] }
+  config.action_mailer.default_url_options = { host: ENV['app_hostname'] }
   config.action_mailer.smtp_settings = {
     address: ENV['relay_address'],
     port: ENV['relay_port'],

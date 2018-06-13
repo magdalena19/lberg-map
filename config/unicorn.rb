c007d9ca7870989b875df0fe0391fa46ff63c601
Paul Kitt <paul.kitt@innoz.de>
2018-06-13 17:13:45 +0200

    Cleaned configs; commented postgres logging

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,6 @@
 app_dir = File.expand_path("../..", __FILE__)
 working_directory app_dir
 
-rails_env = ENV['RAILS_ENV'] || 'production'
 
 # Set unicorn options
 worker_processes 2
@@ -10,8 +9,6 @@ preload_app true
 timeout 30
 
 # Set up socket location
-#listen "/var/run/lbergsocket/unicorn.sockk", :backlog => 64
-#listen "/var/run/unicorn.sock", :backlog => 64
 listen(30000, backlog: 64) #if ENV['RAILS_ENV'] == 'development'
 
 # Logging

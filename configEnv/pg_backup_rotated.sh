072cfb34cb6e672779808cad21ee8d533d067d41
coderat.cc <coderat@systemli.org>
2018-06-13 16:35:52 +0200

    Change backup to type plain and edit create statement in backups

--- configEnv/pg_backup_rotated.sh
+++ configEnv/pg_backup_rotated.sh
@@ -140,7 +140,7 @@ function perform_backups()
     then
       echo "Plain backup of $DATABASE"
 
-      if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
+      if ! pg_dump -Fp -C -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
         echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
       else
         mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
